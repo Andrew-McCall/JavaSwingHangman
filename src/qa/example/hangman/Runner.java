e221f71bf79eb35164e60d91f08a951ca457d00c
Andrew-McCall <amccall@qa.com>
2022-07-18 15:20:23 +0100

    Initial Commit - .gitignore & General Java Project

---
+++ src/qa/example/hangman/Runner.java
@@ -0,0 +1,15 @@
+package qa.example.hangman;
+
+/**
+ * @author Andrew McCall
+ * @version 0.1
+ */
+
+public class Runner {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
